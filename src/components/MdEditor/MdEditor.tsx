a98513e19cd59251c0e521bfa706d5ddccb87e37
单鹏宇 <1285230617@qq.com>
2024-03-16 20:08:30 +0800

    改进了code和md编辑器

--- src/components/MdEditor/MdEditor.tsx
+++ src/components/MdEditor/MdEditor.tsx
@@ -1,11 +1,32 @@
 import gfm from '@bytemd/plugin-gfm'
+import highlightSsr from '@bytemd/plugin-highlight-ssr'
+import highlight from '@bytemd/plugin-highlight'
+import breaks from '@bytemd/plugin-breaks'
+import frontmatter from '@bytemd/plugin-frontmatter'
+import gemoji from '@bytemd/plugin-gemoji'
+import math from '@bytemd/plugin-math'
+import mathSsr from '@bytemd/plugin-math-ssr'
+import mediumZoom from '@bytemd/plugin-medium-zoom'
+import mermaid from '@bytemd/plugin-mermaid'
+
 import {Editor, Viewer} from '@bytemd/react'
 import {useState} from "react";
 
 import 'bytemd/dist/index.css'
+import "github-markdown-css/github-markdown.css";
+import "./MdEditor.less";
 
 const plugins = [
   gfm(),
+  breaks(),
+  frontmatter(),
+  gemoji(),
+  highlight(),
+  highlightSsr(),
+  math(),
+  mathSsr(),
+  mediumZoom(),
+  mermaid()
   // Add more plugins here
 ]
 
@@ -18,13 +39,15 @@ const MdEditor = (props: { value: String, setValue: any }) => {
   } = props;
 
   return (
-    <Editor
-      value={value}
-      plugins={plugins}
-      onChange={(v) => {
-        setValue(v)
-      }}
-    />
+    <div style={{height: "800px"}}>
+      <Editor
+        value={value}
+        plugins={plugins}
+        onChange={(v) => {
+          setValue(v)
+        }}
+      />
+    </div>
   )
 };
 
--- src/components/MdViewer/MdViewer.tsx
+++ src/components/MdViewer/MdViewer.tsx
@@ -1,11 +1,31 @@
 import gfm from '@bytemd/plugin-gfm'
+import highlightSsr from '@bytemd/plugin-highlight-ssr'
+import highlight from '@bytemd/plugin-highlight'
+import breaks from '@bytemd/plugin-breaks'
+import frontmatter from '@bytemd/plugin-frontmatter'
+import gemoji from '@bytemd/plugin-gemoji'
+import math from '@bytemd/plugin-math'
+import mathSsr from '@bytemd/plugin-math-ssr'
+import mediumZoom from '@bytemd/plugin-medium-zoom'
+import mermaid from '@bytemd/plugin-mermaid'
+
 import {Editor, Viewer} from '@bytemd/react'
 import {useState} from "react";
 
 import 'bytemd/dist/index.css'
+import "github-markdown-css/github-markdown.css";
 
 const plugins = [
   gfm(),
+  breaks(),
+  frontmatter(),
+  gemoji(),
+  highlight(),
+  highlightSsr(),
+  math(),
+  mathSsr(),
+  mediumZoom(),
+  mermaid()
   // Add more plugins here
 ]
 
